30325473f7f8ba32fc82771d26622dfc7a14677e
Jorge Adrian Saghin Dudulea <zanajorgesaghin@gmail.com>
2025-06-28 21:11:27 +0200

    los espacios coño

--- install_config.sh
+++ install_config.sh
@@ -6,17 +6,17 @@
 # install both xdg-desktop-portal-hyprland and xdg-desktop-portal-gtk
 
 # Basic packages
-sudo pacman -Syyu hyprland wayland xorg-xwayland libinput libva-nvidia-driver nvidia-utils\
-egl-wayland firefox ghostty mako pipewire wireplumber xdg-desktop-portal-hyprland\
-xdg-desktop-portal-gtk qt5-wayland qt6-wayland hyprpolkitagent sddm uwsm libnewt\
-qt6-svg qt6-virtualkeyboard qt6-multimedia-ffmpeg hypridle hyprlock layer-shell-qt\
+sudo pacman -Syyu hyprland wayland xorg-xwayland libinput libva-nvidia-driver nvidia-utils \
+egl-wayland firefox ghostty mako pipewire wireplumber xdg-desktop-portal-hyprland \
+xdg-desktop-portal-gtk qt5-wayland qt6-wayland hyprpolkitagent sddm uwsm libnewt \
+qt6-svg qt6-virtualkeyboard qt6-multimedia-ffmpeg hypridle hyprlock layer-shell-qt \
 layer-shell-qt5
 
 # Required packages
-sudo pacman -Syyu playerctl waybar ghostty zsh fastfetch wofi xarchiver thunar unrar swww\
-tumbler mpv keepassxc linux-zen linux-zen-headers hyprsunset cmake meson cpio\
-pkgconf gcc libx11 libxcb libxrender xcb-proto libsecret xorg-xhost code gvfs\
-otf-font-awesome hyprshot hyprpicker cliphist networkmanager nm-connection-editor\
+sudo pacman -Syyu playerctl waybar ghostty zsh fastfetch wofi xarchiver thunar unrar swww \
+tumbler mpv keepassxc linux-zen linux-zen-headers hyprsunset cmake meson cpio \
+pkgconf gcc libx11 libxcb libxrender xcb-proto libsecret xorg-xhost code gvfs \
+otf-font-awesome hyprshot hyprpicker cliphist networkmanager nm-connection-editor \
 bluez-utils bluez blueman smbclient gvfs gvfs-smb gvfs-dnssd nss-mdns qalculate-gtk
 
 yay -Syy fsearch qview arttime-git vesktop-bin
