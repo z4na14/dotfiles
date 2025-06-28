2ad25aaf2550f11290eaefd4c06e81c1677e636c
Jorge Adrian Saghin Dudulea <zanajorgesaghin@gmail.com>
2025-06-28 20:34:38 +0200

    time to do laptop

--- install_config.sh
+++ install_config.sh
@@ -15,17 +15,17 @@ required_packages = "playerctl waybar ghostty zsh fastfetch wofi xarchiver thuna
 	tumbler mpv keepassxc linux-zen linux-zen-headers hyprsunset cmake meson cpio\
 	pkgconf gcc libx11 libxcb libxrender xcb-proto libsecret xorg-xhost code gvfs\
 	otf-font-awesome hyprshot hyprpicker cliphist networkmanager nm-connection-editor\
-	bluez-utils bluez blueman smbclient gvfs gvfs-smb gvfs-dnssd nss-mdns qalculate-gtk\
-	kvantum-qt5"
+	bluez-utils bluez blueman smbclient gvfs gvfs-smb gvfs-dnssd nss-mdns qalculate-gtk"
 
-aur_packages = "fsearch qview arttime-git"
-
-# For changing theme, uncomment the following apps
-# sudo pacman -S nwg-look qt5ct qt6ct
-
+aur_packages = "fsearch qview arttime-git vesktop-bin"
 
 sudo pacman -Syyu $basic_packages
 sudo pacman -Syyu $required_packages
+
+# Needed for the theme change
+sudo pacman -Syy nwg-look qt5ct qt6ct
+
+yay -Syy $aur_packages
 
 # Temporal directory for extracts and installations
 rm -fr ./temp
@@ -34,10 +34,10 @@ mkdir ./temp
 
 ### Enable respective services ###
 
-sudo systemctl --user enable --now hyprsunset.service
-sudo systemctl --user enable --now hyprpolkitagent.service
-sudo systemctl --user enable --now hypridle.service
-sudo systemctl --user enable --now NetworkManager.service
+systemctl --user enable --now hyprsunset.service
+systemctl --user enable --now hyprpolkitagent.service
+systemctl --user enable --now hypridle.service
+systemctl --user enable --now NetworkManager.service
 
 
 # Remove firewalld applet
@@ -45,7 +45,8 @@ sudo mv /etc/xdg/autostart/firewall-applet.desktop /etc/xdg/autostart/firewall-a
 
 ### Install Bibata cursor ###
 
-sudo pacman -S xcur2png # hyprcursor-util dependency
+# hyprcursor-util dependency
+sudo pacman -S xcur2png
 
 mkdir -p ~/.local/share/icons
 tar -xf ./cursors/Bibata-Modern-Ice.tar.xz -C ~/.local/share/icons
@@ -59,7 +60,8 @@ sudo ln -s $HOME/.local/share/icons/Bibata-Modern-Ice /usr/share/icons/Bibata-Mo
 sudo mkdir -p /usr/share/icons/default
 
 echo "[Icon Theme]
-Inherits=Bibata-Modern-Ice" > /usr/share/icons/default/index.theme
+Inherits=Bibata-Modern-Ice" > ./temp/cursorconf
+sudo cp ./temp/cursorconf /usr/share/icons/default/index.theme
 
 ### Link and move configurations to their respective places ###
 
@@ -84,3 +86,30 @@ sed -i 's/^Exec=blueman-applet.*/Exec=/' ~/.config/autostart/blueman.desktop
 # Add remove and clear history to drun mode
 sudo cp clear_history.desktop ~/.local/share/applications/clear_history.desktop
 sudo cp remove_history.desktop ~/.local/share/applications/remove_history.desktop
+
+# Move cow file
+sudo cp ./cowsay /usr/share/cowsay/cows
+
+# Vesktop theme
+cp ./vesktop/catppuccin-mocha-sapphire.theme.css ~/.config/vesktop/themes/
+
+# GTK theme
+sudo mv ./gtk/theme /usr/share/themes/catppuccin
+
+# Links (NOT EXPECTED TO WORK ON EVERY MACHINE DUE TO PATHS)
+ln -s $HOME/Descargas/dotfiles/fastfetch $HOME/.config/fastfetch
+ln -s $HOME/Descargas/dotfiles/ghostty $HOME/.config/ghostty
+ln -s $HOME/Descargas/dotfiles/gtk/gtk-3.0 $HOME/.config/gtk-3.0
+ln -s $HOME/Descargas/dotfiles/gtk/gtk-4.0 $HOME/.config/gtk-4.0
+ln -s $HOME/Descargas/dotfiles/hypr $HOME/.config/hypr
+ln -s $HOME/Descargas/dotfiles/mako $HOME/.config/mako
+ln -s $HOME/Descargas/dotfiles/nvim $HOME/.config/nvim
+ln -s $HOME/Descargas/dotfiles/qt/qt5ct $HOME/.config/qt5ct
+ln -s $HOME/Descargas/dotfiles/qt/qt6ct $HOME/.config/qt6ct
+ln -s $HOME/Descargas/dotfiles/spicetify $HOME/.config/spicetify
+ln -s $HOME/Descargas/dotfiles/uwsm $HOME/.config/uwsm
+ln -s $HOME/Descargas/dotfiles/waybar $HOME/.config/waybar
+ln -s $HOME/Descargas/dotfiles/wofi $HOME/.config/wofi
+ln -s $HOME/Descargas/dotfiles/zsh/.zshenv $HOME/.zshenv
+ln -s $HOME/Descargas/dotfiles/zsh/.zshrc $HOME/.zshrc
+ln -s $HOME/Descargas/dotfiles/zsh/oh-my-posh $HOME/.config/oh-my-posh
