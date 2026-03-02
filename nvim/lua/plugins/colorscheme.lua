729a5dcd4ca11d0781864ad321dacef64ef54436
Jorge Adrian Saghin Dudulea <zanajorgesaghin@gmail.com>
2026-03-02 19:38:07 +0100

    things

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,22 @@
 return {
-    { "catppuccin/nvim", name = "catppuccin", lazy = false },
-
-    -- Configure LazyVim to load colorscheme
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        lazy = false,
+        priority = 1000,
+        opts = {
+            transparent_background = true,
+            integrations = {
+                telescope = { enabled = true },
+                mason = true,
+                neotree = true,
+            },
+            styles = {
+                sidebars = "transparent",
+                floats = "transparent",
+            },
+        },
+    },
     {
         "LazyVim/LazyVim",
         opts = {
