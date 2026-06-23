cf81b0a56b7bf4a1d0527d9644dc7187fc2ab976
Jorge Adrian Saghin Dudulea <zanajorgesaghin@gmail.com>
2026-06-23 18:40:30 +0200

    thunderbird theme

--- config/hypr/modules/colors.lua
+++ config/hypr/modules/colors.lua
@@ -1,5 +1,5 @@
 return {
-    image = "/home/z4na14/Documentos/dotfiles/wallpapers/bg5.jpg",
+    image = "/home/z4na14/Documentos/dotfiles/wallpapers/bg9.jpg",
 
     background = "rgba(16, 20, 24, 1)",
 
@@ -9,7 +9,7 @@ return {
 
     inverse_on_surface = "rgba(45, 49, 53, 1)",
 
-    inverse_primary = "rgba(51, 97, 141, 1)",
+    inverse_primary = "rgba(45, 98, 139, 1)",
 
     inverse_surface = "rgba(224, 226, 232, 1)",
 
@@ -19,86 +19,86 @@ return {
 
     on_error_container = "rgba(255, 218, 214, 1)",
 
-    on_primary = "rgba(0, 50, 86, 1)",
+    on_primary = "rgba(0, 51, 82, 1)",
 
-    on_primary_container = "rgba(208, 228, 255, 1)",
+    on_primary_container = "rgba(205, 229, 255, 1)",
 
-    on_primary_fixed = "rgba(0, 29, 53, 1)",
+    on_primary_fixed = "rgba(0, 29, 50, 1)",
 
-    on_primary_fixed_variant = "rgba(21, 73, 116, 1)",
+    on_primary_fixed_variant = "rgba(9, 74, 114, 1)",
 
-    on_secondary = "rgba(36, 49, 64, 1)",
+    on_secondary = "rgba(35, 50, 64, 1)",
 
-    on_secondary_container = "rgba(214, 228, 247, 1)",
+    on_secondary_container = "rgba(212, 228, 246, 1)",
 
-    on_secondary_fixed = "rgba(15, 29, 42, 1)",
+    on_secondary_fixed = "rgba(13, 29, 42, 1)",
 
-    on_secondary_fixed_variant = "rgba(59, 72, 87, 1)",
+    on_secondary_fixed_variant = "rgba(57, 72, 87, 1)",
 
     on_surface = "rgba(224, 226, 232, 1)",
 
-    on_surface_variant = "rgba(194, 199, 207, 1)",
+    on_surface_variant = "rgba(194, 199, 206, 1)",
 
-    on_tertiary = "rgba(58, 42, 72, 1)",
+    on_tertiary = "rgba(56, 42, 74, 1)",
 
-    on_tertiary_container = "rgba(241, 219, 255, 1)",
+    on_tertiary_container = "rgba(237, 220, 255, 1)",
 
-    on_tertiary_fixed = "rgba(36, 21, 50, 1)",
+    on_tertiary_fixed = "rgba(34, 21, 51, 1)",
 
-    on_tertiary_fixed_variant = "rgba(81, 64, 96, 1)",
+    on_tertiary_fixed_variant = "rgba(79, 64, 97, 1)",
 
-    outline = "rgba(140, 145, 153, 1)",
+    outline = "rgba(140, 145, 152, 1)",
 
     outline_variant = "rgba(66, 71, 78, 1)",
 
-    primary = "rgba(158, 202, 252, 1)",
+    primary = "rgba(153, 204, 250, 1)",
 
-    primary_container = "rgba(21, 73, 116, 1)",
+    primary_container = "rgba(9, 74, 114, 1)",
 
-    primary_fixed = "rgba(208, 228, 255, 1)",
+    primary_fixed = "rgba(205, 229, 255, 1)",
 
-    primary_fixed_dim = "rgba(158, 202, 252, 1)",
+    primary_fixed_dim = "rgba(153, 204, 250, 1)",
 
     scrim = "rgba(0, 0, 0, 1)",
 
-    secondary = "rgba(186, 200, 219, 1)",
+    secondary = "rgba(184, 200, 218, 1)",
 
-    secondary_container = "rgba(59, 72, 87, 1)",
+    secondary_container = "rgba(57, 72, 87, 1)",
 
-    secondary_fixed = "rgba(214, 228, 247, 1)",
+    secondary_fixed = "rgba(212, 228, 246, 1)",
 
-    secondary_fixed_dim = "rgba(186, 200, 219, 1)",
+    secondary_fixed_dim = "rgba(184, 200, 218, 1)",
 
     shadow = "rgba(0, 0, 0, 1)",
 
-    source_color = "rgba(42, 86, 127, 1)",
+    source_color = "rgba(160, 202, 242, 1)",
 
     surface = "rgba(16, 20, 24, 1)",
 
     surface_bright = "rgba(54, 57, 62, 1)",
 
-    surface_container = "rgba(29, 32, 36, 1)",
+    surface_container = "rgba(28, 32, 36, 1)",
 
-    surface_container_high = "rgba(39, 42, 47, 1)",
+    surface_container_high = "rgba(39, 42, 46, 1)",
 
-    surface_container_highest = "rgba(50, 53, 58, 1)",
+    surface_container_highest = "rgba(49, 53, 57, 1)",
 
-    surface_container_low = "rgba(25, 28, 32, 1)",
+    surface_container_low = "rgba(24, 28, 32, 1)",
 
-    surface_container_lowest = "rgba(11, 14, 18, 1)",
+    surface_container_lowest = "rgba(11, 15, 18, 1)",
 
     surface_dim = "rgba(16, 20, 24, 1)",
 
-    surface_tint = "rgba(158, 202, 252, 1)",
+    surface_tint = "rgba(153, 204, 250, 1)",
 
     surface_variant = "rgba(66, 71, 78, 1)",
 
-    tertiary = "rgba(213, 190, 229, 1)",
+    tertiary = "rgba(210, 191, 231, 1)",
 
-    tertiary_container = "rgba(81, 64, 96, 1)",
+    tertiary_container = "rgba(79, 64, 97, 1)",
 
-    tertiary_fixed = "rgba(241, 219, 255, 1)",
+    tertiary_fixed = "rgba(237, 220, 255, 1)",
 
-    tertiary_fixed_dim = "rgba(213, 190, 229, 1)",
+    tertiary_fixed_dim = "rgba(210, 191, 231, 1)",
 
 }
